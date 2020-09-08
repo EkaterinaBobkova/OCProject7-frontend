25855e5a51c868bec5b7f0068dc9d98e2f8ac68d
EkaterinaBobkova <63730777+EkaterinaBobkova@users.noreply.github.com>
2020-09-08 19:21:11 +0200

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Connection msg="Connection à l'application"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Connection from '@/components/Connection.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Connection
+  }
+}
+</script>
